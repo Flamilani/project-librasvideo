d63e9733b0c8d18913673fd3f8930f6afdd55a1e
Flavio Milani <milanidesign7@gmail.com>
2022-04-09 19:26:45 -0300

    Implantanção com firebase para ambiente de desenvolvimento

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,22 @@
+const assets = '';
+const imagePath = '../../../../assets/img/logo-3.png';
+const imageSmallPath = '../../../../assets/img/logo-1.png';
+const imageUser = '../../../../assets/img/icon-user.png';
+const imgLoading = '../../../../assets/img/loading.gif';
+
 export const environment = {
-  production: true
+  production: true,
+  assets,
+  imagePath,
+  imageSmallPath,
+  imageUser,
+  imgLoading,
+  firebase: {
+    apiKey: "AIzaSyBirrhMDLCxV67K_Dk1A9ZyBTVHhlrmwJ0",
+    authDomain: "app-librasvideos.firebaseapp.com",
+    projectId: "app-librasvideos",
+    storageBucket: "app-librasvideos.appspot.com",
+    messagingSenderId: "1016087581420",
+    appId: "1:1016087581420:web:ec2da0dbe5b67d9bdb4bc9"
+  }
 };
